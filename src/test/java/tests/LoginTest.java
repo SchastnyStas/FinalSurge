1f4fde889e62aa4d703578d660a4dba7a29c5f17
Stanislau Schastny <stasschastny@gmail.com>
2025-04-22 23:25:24 +0200

    add config

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
-    @Test(testName = "Login user with valid credentials")
+    @Test(description = "Login user with valid credentials")
     public void checkUserLogin() {
         Assert.assertTrue(loginSteps.isCalendarButtonVisible());
     }
 
-    @Test(testName = "Check the logout process")
+    @Test(description = "Check the logout process")
     public void checkUserLogout() {
         Assert.assertTrue(logoutSteps.proceedLogoutAndCheck());
     }
